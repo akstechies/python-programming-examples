06dc980df64c3814b750eb1ac084bb74a3aa73c3
akstechies <akstechies@gmail.com>
2023-07-22 00:28:18 +0530

    variables from interview bit

---
+++ variables.py
@@ -0,0 +1,25 @@
+
+""" # TODO: You have to assign the following values in the variable:
+
+* "InterviewBit" should be assigned in the variable named my_string1.
+* "Don't change the variable name" should be assigned in the variable named my_string2.
+* 11 should be assigned in the variable named my_int.
+* 20.0 should be assigned in the variable named my_float. """
+
+def main():
+    # Assign values to the following variable as described above
+    # Don't change the variable name
+    my_string1 = "InterviewBit"
+    my_string2 = "Don't change the variable name"
+    my_int = 11
+    my_float = 20.0
+    
+    # Don't change the below code
+    print("String1: %s" % my_string1)
+    print("String2: %s" % my_string2)
+    print("Integer: %d" % my_int)
+    print("Float: %f" % my_float)
+    return 0
+
+if __name__ == '__main__':
+    main()
